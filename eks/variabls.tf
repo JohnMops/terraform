323c0e01fbacf25c955be98b1b2067b01a974c46
evgenibi <evgenibi@cloudzone.io>
2020-10-02 11:36:43 +0300

    adding eks tf

---
+++ eks/variabls.tf
@@ -0,0 +1,29 @@
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      userarn  = "arn:aws:iam::535518648590:user/evgenibi"
+      username = "john"
+      groups   = ["system:masters"]
+    }
+  ]
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+
+  ]
+}
